96e6f1ed6410a60a4fe05965bda63818d94bdec9
Johan Andruejol <johan.andruejol@kitware.com>
2014-10-22 17:26:10 -0400

    Add fail unittest to make sure test do fail correctly

---
+++ Applications/App/Testing/Python/FailTests.py
@@ -0,0 +1,62 @@
+import os
+import unittest
+from __main__ import vtk, qt, ctk, slicer
+
+#
+# FailTests
+#
+
+class FailTests:
+  def __init__(self, parent):
+    parent.title = "FailTests" # TODO make this more human readable by adding spaces
+    parent.categories = ["Testing.TestCases"]
+    parent.dependencies = []
+    parent.contributors = ["Johan Andruejol (Kitware)"] # replace with "Firstname Lastname (Org)"
+    parent.helpText = """
+    """
+    parent.acknowledgementText = """TODO""" # replace with organization, grant and thanks.
+    self.parent = parent
+
+    # Add this test to the SelfTest module's list for discovery when the module
+    # is created.  Since this module may be discovered before SelfTests itself,
+    # create the list if it doesn't already exist.
+    try:
+      slicer.selfTests
+    except AttributeError:
+      slicer.selfTests = {}
+    slicer.selfTests['FailTests'] = self.runTest
+
+  def runTest(self):
+    tester = FailTestsTest()
+    tester.runTests()
+
+#
+# FailTestsTest
+#
+
+class FailTestsTest(unittest.TestCase):
+  def test_TestFail(self):
+    self.assertTrue(False)
+
+#
+# FailTestsWidget
+#
+
+class FailTestsWidget():
+  def __init__(self, parent = None):
+    if not parent:
+      self.parent = slicer.qMRMLWidget()
+      self.parent.setLayout(qt.QVBoxLayout())
+      self.parent.setMRMLScene(slicer.mrmlScene)
+    else:
+      self.parent = parent
+    self.layout = self.parent.layout()
+    if not parent:
+      self.setup()
+      self.parent.show()
+
+    self.moduleName = 'FailTests'
+    self.tester = FailTestsTest()
+
+  def setup(self):
+    pass
